eb042cefcd3cb3107da63cd08edabf63de920b2a
Hexuan Li <lihexuan1@gmail.com>
2022-05-24 21:49:07 -0700

    add logic to putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+import { text } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,8 +14,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (id, content) => { 
+  console.log('PUT to the database');
+  //connect to DB and version we want to use
+  const textDb = await openDB('jate',1);
+  //make new transaction , specify the db we posting to and the data privilege of "readwrite"
+  const tx = textDb.transaction('jate','readwrite');
+  //open object store
+  const store = tx.objectStore('jate');
+  //use .put() method to pass in content
+  const request = store.put({ id: id, text: content });
+  //confirm teh data was added
+  const result = await request; console.log('🚀 - data saved to the database', result)
+};
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
