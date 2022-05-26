9158c7977a4441e51cb73cca4a88ba78e26e3b3d
Hexuan Li <lihexuan1@gmail.com>
2022-05-25 17:21:08 -0700

    test the app teh debug in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,29 +1,29 @@
-import { text } from 'express';
+// import { text } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('text', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('text database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('text', { keyPath: 'id', autoIncrement: true });
+      console.log('text database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => { 
+export const putDb = async (content) => { 
   console.log('PUT to the database');
   //connect to DB and version we want to use
-  const textDb = await openDB('jate',1);
+  const textDb = await openDB('text',1);
   //make new transaction , specify the db we posting to and the data privilege of "readwrite"
-  const tx = textDb.transaction('jate','readwrite');
+  const tx = textDb.transaction('text','readwrite');
   //open object store
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore('text');
   //use .put() method to pass in content
-  const request = store.put({ id: id, text: content });
+  const request = store.put({ text: content });
   //confirm teh data was added
   const result = await request; console.log('🚀 - data saved to the database', result)
 };
@@ -31,16 +31,16 @@ export const putDb = async (id, content) => {
 export const getDb = async () => {
   console.log('GET all from the database');
   //connect to DB and version we want to use
-  const todosDb = await openDB('jate', 1);
+  const todosDb = await openDB('text', 1);
   // make new transaction , specify the db we posting to and the data privilege of "readonly"
-  const tx = todosDb.transaction('jate', 'readonly');
+  const tx = todosDb.transaction('text', 'readonly');
   //open the object store
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore('text');
   //use .getAll() method to grab all the content in the DB
   const request = store.getAll();
   //confirm the data was fetched
   const result = await request;
   console.log('result.value', result);
-  return result;
+  // return result.value;
 }
 initdb();
